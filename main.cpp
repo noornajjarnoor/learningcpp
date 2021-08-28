4d413c38dc13021f6cc16cf7bc55c4ff99eb3ce8
noornajjarnoor <88385896+noornajjarnoor@users.noreply.github.com>
2021-08-28 12:57:44 +0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "input.h"
-int main(){
-    int x{ getinteger() };
-	int y{ getinteger() };
- 
-	std::cout << x << " + " << y << " is " << x + y << '\n';
-	return 0;
-}
